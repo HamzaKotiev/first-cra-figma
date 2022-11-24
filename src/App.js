09981c5d531d72ccd35a515b1de4c2c1e2576361
KhamzatKot <1000000000020z@gmail.com>
2022-11-24 14:16:38 +0300

    счетчик сделан

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='header'>
+        <div className='logo'>
+
+          <p className='logoText'>c</p>
+
+          <div className='logoSVG'></div>
+        </div>
+        <div className='menu'>
+          <div className='bar'>
+            <p className='textHome'>Главная</p>
+            <p className='textAboutUs'>О нас </p>
+            <p className='textContacts'>Контакты </p>
+          </div>
+        </div>
+      </div>
+      {/* <hr className='hr' /> */}
+      <div className='main'>
+
+        <p className='numberText'>140</p>
+
+        <div className='buttons'>
+          <div className='increase'>
+            <p className='textIncrease'>Увеличить</p>
+          </div>
+          <div className='decrease'>
+            <p className='textDecrease'>Уменьшить</p>
+          </div>
+          <div className='reset'>
+            <p className='textReset'>Сбросить</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='footer'>
+        <div className='footerLogo'>
+
+        </div>
+        <div className='footerMenu'>
+          <p className='textPartners'>Партнерам</p>
+          <p className='textDevelopers'>Разработчикам</p>
+          <p className='textVacancies'>Вакансии</p>
+        </div>
+        <div className='copyright'>ООО "интукод" 2020г</div>
+      </div>
+
+
+    </>
   );
 }
 
